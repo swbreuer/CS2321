2b66a761f85c819c2990202a2c1439fe84e60c03
swbreuer <swbreuer@mtu.edu>
2020-11-21 14:50:16 -0500

    Implemented AdjList
    Implemented all methods in AdjList
    Lowered timeout for all tests to 150 ms

--- Graph/src/cs2321/AdjListGraph.java
+++ Graph/src/cs2321/AdjListGraph.java
@@ -11,68 +11,96 @@ import net.datastructures.*;
  */
 public class AdjListGraph<V, E> implements Graph<V, E> {
 
+	private boolean directed;
+	
+	private HashMap<edge,E> edgeList;
+	private HashMap<vertex,V> vertexList;
+	
 	public AdjListGraph(boolean directed) {
-		//TODO directed graph constructor 
+		this.directed = directed;
+		edgeList = new HashMap<edge,E>();
+		vertexList = new HashMap<vertex,V>();
 	}
 
 	public AdjListGraph() {
-		//TODO non directed graph constructor 
+		this.directed = false;
+		edgeList = new HashMap<edge,E>();
+		vertexList = new HashMap<vertex,V>();
 	}
 
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#edges()
 	 */
+	@Override
 	public Iterable<Edge<E>> edges() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Edge<E>> list = new ArrayList<Edge<E>>();
+		for(Entry<edge, E> entry : edgeList.entrySet()) {
+			list.addLast(entry.getKey());
+		}
+		return list;
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#endVertices(net.datastructures.Edge)
 	 */
+	@Override
 	public Vertex[] endVertices(Edge<E> e) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		edge edge = cleanEdge(e);
+		return edge.getEndpoints();
 	}
 
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#insertEdge(net.datastructures.Vertex, net.datastructures.Vertex, java.lang.Object)
 	 */
+	@Override
 	public Edge<E> insertEdge(Vertex<V> u, Vertex<V> v, E o)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		if(getEdge(u,v)==null) {
+			edge e = new edge(u,v,o);
+			vertex w = cleanVertex(u);
+			vertex x = cleanVertex(v);
+			w.getOutgoing().put(x, e);
+			x.getIncoming().put(w, e);
+			edgeList.put(e, o);
+			return e;
+		}
+		else {
+			throw new IllegalArgumentException("edge already exists between " + u + " and " + v);
+		}
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#insertVertex(java.lang.Object)
 	 */
+	@Override
 	public Vertex<V> insertVertex(V o) {
-		// TODO Auto-generated method stub
-		return null;
+		vertex v = new vertex(o);
+		vertexList.put(v, o);
+		return v;
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#numEdges()
 	 */
+	@Override
 	public int numEdges() {
-		// TODO Auto-generated method stub
-		return 0;
+		return edgeList.size();
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#numVertices()
 	 */
+	@Override
 	public int numVertices() {
-		// TODO Auto-generated method stub
-		return 0;
+		return vertexList.size();
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#opposite(net.datastructures.Vertex, net.datastructures.Edge)
 	 */
+	@Override
 	public Vertex<V> opposite(Vertex<V> v, Edge<E> e)
 			throws IllegalArgumentException {
 		// TODO Auto-generated method stub
@@ -82,16 +110,30 @@ public class AdjListGraph<V, E> implements Graph<V, E> {
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#removeEdge(net.datastructures.Edge)
 	 */
+	@Override
 	public void removeEdge(Edge<E> e) throws IllegalArgumentException {
-
+		edge edge = cleanEdge(e);
+		edgeList.remove(edge);
+		Vertex[] vertlist = edge.getEndpoints();
+		vertex outgo = cleanVertex(vertlist[0]);
+		vertex incom = cleanVertex(vertlist[1]);
+		outgo.getOutgoing().remove(incom);
+		incom.getIncoming().remove(outgo);
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#removeVertex(net.datastructures.Vertex)
 	 */
+	@Override
 	public void removeVertex(Vertex<V> v) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-	
+		vertex vert = cleanVertex(v);
+		vertexList.remove(vert);
+		for(Entry<vertex,edge> entry : vert.getOutgoing().entrySet()) {
+			removeEdge(entry.getValue());
+		}
+		for(Entry<vertex,edge> entry : vert.getIncoming().entrySet()) {
+			removeEdge(entry.getValue());
+		}
 	}
 
 	/* 
@@ -113,42 +155,108 @@ public class AdjListGraph<V, E> implements Graph<V, E> {
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#vertices()
 	 */
+	@Override
 	public Iterable<Vertex<V>> vertices() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Vertex<V>> list = new ArrayList<Vertex<V>>();
+		for(Entry<vertex, V> entry : vertexList.entrySet()) {
+			list.addLast(entry.getKey());
+		}
+		return list;
 	}
 
 	@Override
 	public int outDegree(Vertex<V> v) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return 0;
+		vertex vert = cleanVertex(v);
+		return vert.getOutgoing().size();
 	}
 
 	@Override
 	public int inDegree(Vertex<V> v) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return 0;
+		vertex vert = cleanVertex(v);
+		return vert.getIncoming().size();
 	}
 
 	@Override
 	public Iterable<Edge<E>> outgoingEdges(Vertex<V> v)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		vertex vert = cleanVertex(v);
+		ArrayList<Edge<E>> edges = new ArrayList<Edge<E>>();
+		for(Entry<vertex,edge> entry : vert.getOutgoing().entrySet()) {
+			edges.addLast(entry.getValue());
+		}
+		return edges;
 	}
 
 	@Override
 	public Iterable<Edge<E>> incomingEdges(Vertex<V> v)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		vertex vert = cleanVertex(v);
+		ArrayList<Edge<E>> edges = new ArrayList<Edge<E>>();
+		for(Entry<vertex,edge> entry : vert.getIncoming().entrySet()) {
+			edges.addLast(entry.getValue());
+		}
+		return edges;
 	}
 
 	@Override
 	public Edge<E> getEdge(Vertex<V> u, Vertex<V> v)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
+	private edge cleanEdge(Edge<E> e) throws IllegalArgumentException{
+		edge edge = (edge) e;
+		return edge;
+	}
+	
+	private vertex cleanVertex(Vertex<V> v) throws IllegalArgumentException{
+		vertex vertex = (vertex) v;
+		return vertex;
+	}
+	
+	private class vertex implements Vertex<V>{
+		private V element;
+		private HashMap<vertex,edge> incoming;
+		private HashMap<vertex,edge> outgoing;
+		public vertex(V element) {
+			this.element = element;
+			outgoing = new HashMap<vertex,edge>();
+			if(directed) {
+				incoming = new HashMap<vertex,edge>();
+			}
+			else {
+				incoming = outgoing;
+			}
+		}
+		@Override
+		public V getElement() {
+			return element;
+		}
+		
+		public HashMap<vertex,edge> getOutgoing(){
+			return outgoing;
+		}
+		public HashMap<vertex,edge> getIncoming(){
+			return incoming;
+		}
+	}
+	
+	private class edge implements Edge<E>{
+		private E element;
+		private Vertex<V>[] endpoints;
+		
+		public edge(Vertex<V> w, Vertex<V> x, E element) {
+			this.element = element;
+			endpoints = new Vertex[]{w,x};
+		}
+		
+		@Override
+		public E getElement() {
+			return element;
+		}
+		
+		public Vertex<V>[] getEndpoints(){
+			return endpoints;
+		}
+	}
 }
--- Graph/tests/AdjListGraph/DirectedGraphTest.java
+++ Graph/tests/AdjListGraph/DirectedGraphTest.java
@@ -18,7 +18,7 @@ public class DirectedGraphTest {
 		TARGET = new AdjListGraph(true);
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying insertVertex method")
 	public void Test1() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("test");
@@ -26,7 +26,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying insertVertex method", (Object)(false), (Object)(v1.getElement()==null));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying insertEdge method")
 	public void Test2() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -36,7 +36,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying insertEdge method", (Object)(false), (Object)(e1.getElement()==null));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying removeVertex method")
 	public void Test3() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -46,7 +46,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying removeVertex method", (Object)(0), (Object)(TARGET.outDegree(v1)));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying removeEdge method")
 	public void Test4() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -57,7 +57,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying removeEdge method", (Object)(0), (Object)(TARGET.numEdges()));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying edges() size equals 10")
 	public void Test5() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -83,7 +83,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying edges() size equals 10", (Object)(10), (Object)(size));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying numEdges() size equals 10")
 	public void Test6() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -105,7 +105,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying numEdges() size equals 10", (Object)(10), (Object)(TARGET.numEdges()));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying vertices() size equals 5")
 	public void Test7() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -131,7 +131,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying vertices() size equals 5", (Object)(5), (Object)(size));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying numVertices() size equals 1")
 	public void Test8() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -153,7 +153,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying numVertices() size equals 1", (Object)(5), (Object)(TARGET.numVertices()));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying incomingEdges(\"alice\") size equals 0")
 	public void Test9() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -179,7 +179,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying incomingEdges(\"alice\") size equals 0", (Object)(0), (Object)(size));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying incomingEdges(\"bob\") size equals 1")
 	public void Test10() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -205,7 +205,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying incomingEdges(\"bob\") size equals 1", (Object)(1), (Object)(size));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying inDegree in (\"alice\") equals 0")
 	public void Test11() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -227,7 +227,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying inDegree in (\"alice\") equals 0", (Object)(0), (Object)(TARGET.inDegree(v1)));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying inDegree in (\"bob\") equals 1")
 	public void Test12() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -249,7 +249,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying inDegree in (\"bob\") equals 1", (Object)(1), (Object)(TARGET.inDegree(v2)));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying outDegree in (\"alice\") equals 4")
 	public void Test13() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -271,7 +271,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying outDegree in (\"alice\") equals 4", (Object)(4), (Object)(TARGET.outDegree(v1)));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying outDegree in (\"bob\") equals 3")
 	public void Test14() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -293,7 +293,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying outDegree in (\"bob\") equals 3", (Object)(3), (Object)(TARGET.outDegree(v2)));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying outgoingEdges(\"alice\") size equals 4")
 	public void Test15() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
@@ -319,7 +319,7 @@ public class DirectedGraphTest {
 		org.junit.Assert.assertEquals("Verifying outgoingEdges(\"alice\") size equals 4", (Object)(4), (Object)(size));
 	}
 
-	@org.junit.Test(timeout=15000)
+	@org.junit.Test(timeout=150)
 	@jug.TestName("Verifying outgoingEdges(\"bob\") size equals 3")
 	public void Test16() throws Throwable {
 		Vertex<String> v1 = TARGET.insertVertex("alice");
